f644a052d7ce24ae5c92b90deb22e8e48b1157ca
Maxim Yakovlev <it.trainee@yandex.ru>
2024-12-18 00:27:23 +0300

    added documentation for irq_handler function using the kerneldoc format

--- module.c
+++ module.c
@@ -2,6 +2,19 @@
 #include <linux/module.h>
 #include <linux/interrupt.h>
 
+/**
+ * irq_handler - Interrupt handler for a device
+ *
+ * @irq: The interrupt number that triggered the handler
+ * @dev_id: A pointer to device-specific data (typically passed during IRQ setup)
+ *
+ * This function is a simple interrupt handler that logs a message
+ * indicating that an interrupt has been handled for the specified device.
+ * The device information is retrieved from the @dev_id parameter.
+ *
+ * Return: Always returns IRQ_HANDLED to indicate that the interrupt
+ *         was successfully handled.
+ */
 irqreturn_t irq_handler(int irq, void *dev_id)
 {
     printk(KERN_INFO "interrupt handled for device %s!\n", (char *)dev_id);
